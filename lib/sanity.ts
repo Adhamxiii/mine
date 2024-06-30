db206c998e4f584a89bc45dacf471e1f1dc894ea
Adham Nasser <adhamxiii22@gmail.com>
2024-06-30 22:49:21 +0300

    fix some bugs 🐞

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,12 +1,12 @@
-import { createClient } from "next-sanity";
+// import { createClient } from "next-sanity";
 
-const projectId = "e6hbw1zh";
-const dataset = "production";
-const apiVersion = "v2022-03-07";
+// const projectId = "e6hbw1zh";
+// const dataset = "production";
+// const apiVersion = "v2022-03-07";
 
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true,
-});
+// export const client = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: true,
+// });
