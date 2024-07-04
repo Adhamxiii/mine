bed89d4176ef58deda6d95ec88d945f649fcda7e
Adham Nasser <adhamxiii22@gmail.com>
2024-07-04 09:38:47 +0300

    update ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,26 +4,27 @@ import { Inter } from "next/font/google";
 import Navbar from "../components/Navbar";
 import Provider from "../components/Provider";
 import "./globals.css";
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://adhamz.vercel.app/'),
+  metadataBase: new URL("https://adhamz.vercel.app/"),
   title: {
     default: "Adham Z - Web Developer Portfolio",
-    template: 'Adham Z - %s'
+    template: "Adham Z - %s",
   },
   description:
     "Welcome to Adham Z's portfolio! Discover web development projects, creative designs, and professional experiences that highlight my skills and expertise in building modern, responsive websites.",
   openGraph: {
-    title: 'Adham Nasser',
-    description: "Welcome to Adham Z's portfolio! Discover web development projects, creative designs, and professional experiences that highlight my skills and expertise in building modern, responsive websites.",
+    title: "Adham Nasser",
+    description:
+      "Welcome to Adham Z's portfolio! Discover web development projects, creative designs, and professional experiences that highlight my skills and expertise in building modern, responsive websites.",
     type: "profile",
     locale: "en_US",
     url: "https://adhamz.vercel.app/",
-    siteName: "AdhamZ"
-  }
+    siteName: "AdhamZ",
+  },
 };
 
 export default function RootLayout({
@@ -35,12 +36,12 @@ export default function RootLayout({
     <SessionWrapper>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={`${inter.className} bg-white text-black dark:bg-[#090908] dark:text-white h-full selection:bg-gray-200 dark:selection:bg-gray-800`}
+          className={`${inter.className} h-full bg-white text-black selection:bg-gray-200 dark:bg-[#090908] dark:text-white dark:selection:bg-gray-800`}
         >
           <AntdRegistry>
             <Provider>
               <Navbar />
-              <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 {children}
               </main>
             </Provider>
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { ConfigProvider, Timeline } from "antd";
 import Image from "next/image";
 import { Suspense, useEffect, useState } from "react";
 
-
 const socialLinks = [
   {
     name: "GitHub",
@@ -17,7 +16,7 @@ const socialLinks = [
       <svg
         viewBox="0 0 1024 1024"
         fill="currentColor"
-        className="w-8 h-8 text-red-500 hover:text-red-600"
+        className="h-8 w-8 text-red-500 hover:text-red-600"
       >
         <path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z" />
       </svg>
@@ -30,7 +29,7 @@ const socialLinks = [
       <svg
         viewBox="0 0 1024 1024"
         fill="currentColor"
-        className="w-8 h-8 text-red-500 hover:text-red-600"
+        className="h-8 w-8 text-red-500 hover:text-red-600"
       >
         <path d="M880 112H144c-17.7 0-32 14.3-32 32v736c0 17.7 14.3 32 32 32h736c17.7 0 32-14.3 32-32V144c0-17.7-14.3-32-32-32zM349.3 793.7H230.6V411.9h118.7v381.8zm-59.3-434a68.8 68.8 0 1168.8-68.8c-.1 38-30.9 68.8-68.8 68.8zm503.7 434H675.1V608c0-44.3-.8-101.2-61.7-101.2-61.7 0-71.2 48.2-71.2 98v188.9H423.7V411.9h113.8v52.2h1.6c15.8-30 54.5-61.7 112.3-61.7 120.2 0 142.3 79.1 142.3 181.9v209.4z" />
       </svg>
@@ -43,7 +42,7 @@ const socialLinks = [
       <svg
         viewBox="0 0 1024 1024"
         fill="currentColor"
-        className="w-8 h-8 text-red-500 hover:text-red-600"
+        className="h-8 w-8 text-red-500 hover:text-red-600"
       >
         <path d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zm-80.8 108.9L531.7 514.4c-7.8 6.1-18.7 6.1-26.5 0L189.6 268.9A7.2 7.2 0 01194 256h648.8a7.2 7.2 0 014.4 12.9z" />
       </svg>
@@ -61,38 +60,38 @@ export default function Home() {
 
   useEffect(() => {
     const getSkillsDetails = async () => {
-      const { skills } = await getSkills()
-      setSkills(skills)
-      setLoading(false)
-    }
+      const { skills } = await getSkills();
+      setSkills(skills);
+      setLoading(false);
+    };
     const getExperienceDetails = async () => {
-      const { experience } = await getExperience()
-      setExperience(experience)
-      setLoading(false)
-    }
+      const { experience } = await getExperience();
+      setExperience(experience);
+      setLoading(false);
+    };
     const getEducationDetails = async () => {
-      const { education } = await getEducation()
-      setEducation(education)
-      setLoading(false)
-    }
-    getSkillsDetails()
-    getExperienceDetails()
-    getEducationDetails()
+      const { education } = await getEducation();
+      setEducation(education);
+      setLoading(false);
+    };
+    getSkillsDetails();
+    getExperienceDetails();
+    getEducationDetails();
   }, []);
 
   return (
     <div className="divide-y divide-gray-100 dark:divide-gray-700">
-      <div className="pt-5 pb-8 space-y-2 md:space-x-5">
-        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+      <div className="space-y-2 pb-8 pt-5 md:space-x-5">
+        <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl">
           Home
         </h1>
       </div>
 
       <div className="relative space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-6 xl:space-y-0">
-        <div className="flex flex-col items-center pt-8 h-fit sticky top-10 max-lg:relative max-lg:top-0">
+        <div className="sticky top-10 flex h-fit flex-col items-center pt-8 max-lg:relative max-lg:top-0">
           <Suspense
             fallback={
-              <Skeleton className="w-48 h-48 rounded-full object-cover object-top" />
+              <Skeleton className="h-48 w-48 rounded-full object-cover object-top" />
             }
           >
             <Image
@@ -100,11 +99,11 @@ export default function Home() {
               alt="Picture of me"
               width={200}
               height={200}
-              className="w-48 h-48 rounded-full object-cover object-top"
+              className="h-48 w-48 rounded-full object-cover object-top"
             />
           </Suspense>
 
-          <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
+          <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight text-gray-900 dark:text-gray-100">
             Adham Nasser
           </h3>
 
@@ -119,8 +118,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="prose max-w-none prose-lg pt-8 pb-7 dark:prose-invert xl:col-span-2">
-
+        <div className="prose prose-lg max-w-none pb-7 pt-8 dark:prose-invert xl:col-span-2">
           <p>
             Hey everyone! 👋 I'm Adham Nasser, a front-end developer with a
             knack for crafting engaging experiences using React.
@@ -129,114 +127,161 @@ export default function Home() {
           <p>
             I'm passionate about building scalable and performant web
             applications. My journey in software development began with my love
-            for web development and I've since honed my skills in the
-            industry.
+            for web development and I've since honed my skills in the industry.
           </p>
 
           <p>
             I'm a quick learner and love to stay up-to-date with the latest
-            industry trends and technologies. I'm excited to bring my
-            skills and passion to your next project.
+            industry trends and technologies. I'm excited to bring my skills and
+            passion to your next project.
           </p>
 
           <div>
-            <h2 className="max-lg:text-center">My <span className="text-primary">Skills</span></h2>
-            {loading ? <div className="flex justify-center items-center">
-              <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
-            </div> : (
+            <h2 className="max-lg:text-center">
+              My <span className="text-primary">Skills</span>
+            </h2>
+            {loading ? (
+              <div className="flex items-center justify-center">
+                <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
+              </div>
+            ) : (
               <>
-                {!skills || skills.length === 0 ? <p className="text-center">No skills found</p>
-                  : <div className="scrollbar-hide overflow-x-scroll px-4">
-                    <div className="flex gap-10 max-h-fit">
+                {!skills || skills.length === 0 ? (
+                  <p className="text-center">No skills found</p>
+                ) : (
+                  <div className="">
+                    <div className="flex max-h-fit flex-wrap gap-4">
                       {skills.map((skill: Skill) => (
-                        <div key={skill._id} className="h-fit border border-secondary-foreground dark:border-red-700 px-10 py-5 rounded-md bg-transparent text-black dark:text-white ">
-                          <p className="text-center text-xl font-semibold m-0">{skill.skill}</p>
+                        <div
+                          key={skill._id}
+                          className="h-fit rounded-full border border-secondary-foreground bg-transparent px-5 py-2 text-black dark:border-red-700 dark:text-white"
+                        >
+                          <p className="m-0 text-center text-sm font-semibold">
+                            {skill.skill}
+                          </p>
                         </div>
                       ))}
                     </div>
-                  </div>}
+                  </div>
+                )}
               </>
             )}
           </div>
 
-          <div className="flex gap-4 flex-col">
+          <div className="flex flex-col gap-4">
             <div className="flex-1">
-              <h2 className="max-lg:text-center">My <span className="text-primary">Experience</span></h2>
-              {loading ? <div className="flex justify-center items-center">
-                <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
-              </div> : (
+              <h2 className="max-lg:text-center">
+                My <span className="text-primary">Experience</span>
+              </h2>
+              {loading ? (
+                <div className="flex items-center justify-center">
+                  <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
+                </div>
+              ) : (
                 <>
-                  {!experience || experience.length === 0 ?
-                    <p className="text-center">No experience found</p> :
+                  {!experience || experience.length === 0 ? (
+                    <p className="text-center">No experience found</p>
+                  ) : (
                     <ConfigProvider
                       theme={{
                         components: {
                           Timeline: {
-                            tailColor: 'rgba(255, 0, 0, 0.2)'
-                          }
-                        }
+                            tailColor: "rgba(255, 0, 0, 0.2)",
+                          },
+                        },
                       }}
                     >
                       <Timeline mode="alternate" reverse>
                         {experience.map((e: Experience) => (
-                          <Timeline.Item key={e._id} color="red" >
-                            <div className="border border-primary px-3 py-5 rounded-lg flex flex-col items-start text-black dark:text-white">
+                          <Timeline.Item key={e._id} color="red">
+                            <div className="flex flex-col items-start rounded-lg border border-primary px-3 py-5 text-black dark:text-white">
                               <p className="text-md my-1">{e.duration}</p>
-                              <p className="text-xl font-bold my-1 text-start">{e.company}</p>
-                              <p className="text-md my-1 text-start">Position: <span className="font-bold text-xl text-muted-foreground">{e.position}</span></p>
-                              <p className="text-md my-1 text-start">Location: <span className="font-bold text-muted-foreground">{e.location}</span></p>
-                              <p className="text-md my-1 text-start">Job Profile: <span className="font-bold text-muted-foreground">{e.jobprofile}</span></p>
+                              <p className="my-1 text-start text-xl font-bold">
+                                {e.company}
+                              </p>
+                              <p className="text-md my-1 text-start">
+                                Position:{" "}
+                                <span className="text-xl font-bold text-muted-foreground">
+                                  {e.position}
+                                </span>
+                              </p>
+                              <p className="text-md my-1 text-start">
+                                Location:{" "}
+                                <span className="font-bold text-muted-foreground">
+                                  {e.location}
+                                </span>
+                              </p>
+                              <p className="text-md my-1 text-start">
+                                Job Profile:{" "}
+                                <span className="font-bold text-muted-foreground">
+                                  {e.jobprofile}
+                                </span>
+                              </p>
                             </div>
                           </Timeline.Item>
                         ))}
                       </Timeline>
                     </ConfigProvider>
-                  }
+                  )}
                 </>
               )}
-
             </div>
 
             <div className="flex-1">
-              <h2 className="max-lg:text-center">My <span className="text-primary">Education</span></h2>
-              {loading ? <div className="flex justify-center items-center">
-                <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
-              </div> : (
+              <h2 className="max-lg:text-center">
+                My <span className="text-primary">Education</span>
+              </h2>
+              {loading ? (
+                <div className="flex items-center justify-center">
+                  <div className="h-12 w-12 animate-spin rounded-full border-t-4 border-solid border-red-500"></div>
+                </div>
+              ) : (
                 <>
-                  {!education || education.length === 0 ?
-                    <p className="text-center">No education found</p> :
+                  {!education || education.length === 0 ? (
+                    <p className="text-center">No education found</p>
+                  ) : (
                     <ConfigProvider
                       theme={{
                         components: {
                           Timeline: {
-                            tailColor: 'rgba(255, 0, 0, 0.2)'
-                          }
-                        }
+                            tailColor: "rgba(255, 0, 0, 0.2)",
+                          },
+                        },
                       }}
                     >
                       <Timeline mode="right">
                         {education.map((e: Education) => (
-                          <Timeline.Item key={e._id} color="gray" >
-                            <div className="border border-gray-500 px-3 py-5 rounded-lg flex flex-col items-start text-black dark:text-white">
-                              <p className="text-lg font-bold my-1">{e.college}</p>
-                              <div className="flex w-full justify-between items-center">
-                                <p>Year: <span className="font-bold text-muted-foreground">{e.year}</span></p>
-                                <p>Degree: <span className="font-bold text-muted-foreground">{e.degree}</span></p>
+                          <Timeline.Item key={e._id} color="gray">
+                            <div className="flex flex-col items-start rounded-lg border border-gray-500 px-3 py-5 text-black dark:text-white">
+                              <p className="my-1 text-lg font-bold">
+                                {e.college}
+                              </p>
+                              <div className="flex w-full items-center justify-between">
+                                <p>
+                                  Year:{" "}
+                                  <span className="font-bold text-muted-foreground">
+                                    {e.year}
+                                  </span>
+                                </p>
+                                <p>
+                                  Degree:{" "}
+                                  <span className="font-bold text-muted-foreground">
+                                    {e.degree}
+                                  </span>
+                                </p>
                               </div>
                             </div>
                           </Timeline.Item>
                         ))}
                       </Timeline>
                     </ConfigProvider>
-                  }
+                  )}
                 </>
               )}
             </div>
           </div>
-
         </div>
       </div>
-
     </div>
   );
 }
