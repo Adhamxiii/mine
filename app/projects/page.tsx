71fc672f7d80af764237d827ea6c3c63a8886466
Adham Nasser <adhamxiii22@gmail.com>
2024-08-24 19:43:29 +0300

    update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -56,7 +56,7 @@ const ProjectsPage = () => {
     <Loader />
   ) : (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      <div className="flex items-center justify-between space-y-2 pb-8 pt-6 md:space-y-5">
+      <div className="flex items-center justify-between gap-4 space-y-2 pb-8 pt-6 max-md:flex-col md:space-y-5">
         <h1 className="md:leading-14 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl">
           Projects
         </h1>
